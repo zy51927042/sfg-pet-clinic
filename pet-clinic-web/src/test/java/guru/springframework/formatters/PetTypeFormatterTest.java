e911c2f2e73f0094a3dbdefd0d2da978b3b10984
zy51927042 <zy51927042@163.com>
2022-05-26 18:04:33 +0800

    Add Pet controller and the html templates.closes #56

---
+++ pet-clinic-web/src/test/java/guru/springframework/formatters/PetTypeFormatterTest.java
@@ -0,0 +1,10 @@
+package guru.springframework.formatters;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class PetTypeFormatterTest {
+
+}
